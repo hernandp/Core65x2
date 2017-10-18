2fd856d263ecb89945a04b73446bc81e2470aadf
Hernán Di Pietro <hernan.di.pietro@gmail.com>
2017-10-18 14:07:23 -0300

    More opcodes and code cleanup

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -20,57 +20,33 @@ pub const FLAG_BRK: u8 = 0b0001_0000;
 pub const FLAG_OF: u8 = 0b0100_0000;
 pub const FLAG_SIGN: u8 = 0b1000_0000;
 
-// 
+//
 // Vector adresses
 //
 const VECTOR_NMI: u16 = 0xFFFA;
-const VECTOR_RESET:u16= 0xFFFC;
-const VECTOR_IRQ_BRK:  u16= 0xFFFE;
+const VECTOR_RESET: u16 = 0xFFFC;
+const VECTOR_IRQ_BRK: u16 = 0xFFFE;
 
 
 //
 // Addressing mode modifier for opcodes
 //
 pub enum AddrMode {
-    Acc,    // Accumulator
+    Acc, // Accumulator
     Impl,
-    Imm,    // LDA #$22   Immediate
-    ZP,     // LDY $02      Zero-page
-    ZPX,    // LDA $00,X    Zero-page indexed (X)
-    ZPY,    // LDA $00,Y    Zero-page indexed (Y)
+    Imm, // LDA #$22   Immediate
+    ZP, // LDY $02      Zero-page
+    ZPX, // LDA $00,X    Zero-page indexed (X)
+    ZPY, // LDA $00,Y    Zero-page indexed (Y)
     ZPIndX, // ($00,X)      Zero-page indexed indirect X
     ZPIndY, // ($00),Y      Zero-page indexed indirect Y
-    Abs,    // LDX $0000    Absolute
-    AbsX,   // ADC $C000,X  Absolute indexed with X
-    AbsY,   // INC $F000,Y  Absolute indexed with Y
-    Ind,    // ($0000)      Indirect
-    Rel,    // PC + $00
+    Abs, // LDX $0000    Absolute
+    AbsX, // ADC $C000,X  Absolute indexed with X
+    AbsY, // INC $F000,Y  Absolute indexed with Y
+    Ind, // ($0000)      Indirect
+    Rel, // PC + $00
 }
 
-// Register modifier for opcodes
-#[derive(PartialEq)]
-pub enum RegMod {
-    None,
-    A,
-    X,
-    Y,
-    SP,
-    SR,
-}
-
-// Instruction R/W type for address calculation
-enum InsMemAccess {
-    Read,
-    ReadWrite,
-    Write,
-    Jump
-}
-
-// Effective address: (address, clock count)
-struct EAResult {
-    addr: u16,
-    clk_count: u64,
-}
 
 // 6502 CPU registers
 pub struct Regs {
@@ -93,7 +69,7 @@ pub struct Cpu<'a> {
     pub regs: Regs,
     pub mem: &'a mut Memory,
     clk_count: u64,
-    icd: InstrCycleData, 
+    icd: InstrCycleData,
 }
 
 // macro_rules! set_zf { ($v:expr) => ( if $v == 0 { self.regs.SR |= FLAG_ZERO; } else { self.regs.SR &= !FLAG_ZERO; } ) }
@@ -117,19 +93,22 @@ impl<'a> Cpu<'a> {
             mem: sysmem,
             icd: InstrCycleData {
                 opcode: 0x00,
-                ea: 0x0000, 
+                ea: 0x0000,
                 op0: 0x00,
-                op1: 0x00
-            }
+                op1: 0x00,
+            },
         }
     }
 
 
-    // 
+    //
     // Do the RESET cycle for the emulated processor
     //
     pub fn reset(&mut self) {
-        self.regs.PC = self.addr_from_2b(self.mem.read_byte(VECTOR_RESET),self.mem.read_byte(VECTOR_RESET + 1));
+        self.regs.PC = self.addr_from_2b(
+            self.mem.read_byte(VECTOR_RESET),
+            self.mem.read_byte(VECTOR_RESET + 1),
+        );
         self.regs.SP = 0xFD;
         self.clk_count = 7;
     }
@@ -138,13 +117,13 @@ impl<'a> Cpu<'a> {
     // Execute instruction at current program counter.
     // Returns elapsed clock cycles.
     //
-    pub fn exec(&mut self)  {
+    pub fn exec(&mut self) {
 
         // Fetch instruction, operands and calculate effective address
 
-        self.fetch_instr();                
+        self.fetch_instr();
         let addr_m = &OPCODE_TABLE[self.icd.opcode as usize].addr_m;
-        
+
         self.fetch_op(&addr_m);
         self.calc_eff_addr(&addr_m);
 
@@ -152,132 +131,121 @@ impl<'a> Cpu<'a> {
 
         self.clk_count += CLK_TABLE[self.icd.opcode as usize];
 
-
         match OPCODE_TABLE[self.icd.opcode as usize].name {
-
             "BRK" => {
                 println!("BRK");
             }
             "LDA" => {
-                 let v = self.get_src_value(&addr_m);
-                 self.set_s_flag(v);
-                 self.set_z_flag(v);
-                 self.regs.A = v;
-            },
+                let v = self.get_src_value(&addr_m);
+                self.set_nz_flags(v);
+                self.regs.A = v;
+            }
             "LDX" => {
                 let v = self.get_src_value(&addr_m);
-                self.set_s_flag(v);
-                self.set_z_flag(v);
+                self.set_nz_flags(v);
                 self.regs.X = v;
-            },
+            }
             "LDY" => {
                 let v = self.get_src_value(&addr_m);
-                self.set_s_flag(v);
-                self.set_z_flag(v);
+                self.set_nz_flags(v);
                 self.regs.Y = v;
-            },
+            }
             "STA" => self.mem.write_byte(self.icd.ea, self.regs.A),
             "STX" => self.mem.write_byte(self.icd.ea, self.regs.X),
             "STY" => self.mem.write_byte(self.icd.ea, self.regs.Y),
-            "TXA" => { 
+            "TXA" => {
                 let v = self.regs.X;
-                self.regs.A = v; 
-                self.set_s_flag(v);
-                self.set_z_flag(v);
-            },
-            "TAX" => { 
+                self.regs.A = v;
+                self.set_nz_flags(v);
+            }
+            "TAX" => {
                 let v = self.regs.A;
                 self.regs.X = v;
-                self.set_s_flag(v);
-                self.set_z_flag(v);
-            },
-            "TYA" => { 
+                self.set_nz_flags(v);
+            }
+            "TYA" => {
                 let v = self.regs.A;
                 self.regs.Y = v;
-                self.set_s_flag(v);
-                self.set_z_flag(v);
-            },
-            "TAY" => { 
+                self.set_nz_flags(v);
+            }
+            "TAY" => {
                 let v = self.regs.Y;
-                self.regs.A = v; 
-                self.set_s_flag(v);
-                self.set_z_flag(v);
-            },
-            "TSX" => { 
+                self.regs.A = v;
+                self.set_nz_flags(v);
+            }
+            "TSX" => {
                 let v = self.regs.X;
                 self.regs.X = v;
-                self.set_s_flag(v);
-                self.set_z_flag(v); 
-            },
+                self.set_nz_flags(v);
+            }
             "TXS" => {
                 let v = self.regs.SP;
                 self.regs.SP = v;
-                self.set_s_flag(v);
-                self.set_z_flag(v);
-            },
+                self.set_nz_flags(v);
+            }
+            "DEC" => {
+                let v = self.get_src_value(&addr_m).wrapping_sub(1);
+                self.set_nz_flags(v);
+                self.mem.write_byte(self.icd.ea, v);
+            }
+            "DEX" => {
+                let v = self.regs.X.wrapping_sub(1);
+                self.set_nz_flags(v);
+                self.regs.X = v;
+            }
+            "DEY" => {
+                let v = self.regs.Y.wrapping_sub(1);
+                self.set_nz_flags(v);
+                self.regs.Y = v;
+            }
+            "INC" => {
+                let v = self.get_src_value(&addr_m).wrapping_add(1);
+                self.set_nz_flags(v);
+                self.mem.write_byte(self.icd.ea, v);
+            }
+            "INY" => {
+                let v = self.regs.Y.wrapping_add(1);
+                self.set_nz_flags(v);
+                self.regs.Y = v;
+            }
+            "INX" => {
+                let v = self.regs.Y.wrapping_add(1);
+                self.set_nz_flags(v);
+                self.regs.Y = v;
+            }
+            "JMP" => {
+                self.regs.PC = self.icd.ea;
+            }
+            "NOP" => {
+                // do nothing...
+            }
+            "CMP" => {
+                let v = self.get_src_value(&addr_m);
+                let cmps: u16 = v as u16 - self.regs.A as u16;
+                self.set_c_flag(cmps < 0x100);
+                self.set_nz_flags(cmps as u8);
+            }
             _ => {
                 println!("OTHER");
             }
         }
-
-
-/*
-        let clk: u64 = match opcode_table[self.icd.opcode as usize].ins {
-            InsGrp::Adc => self.op_adc(addr_m),
-            InsGrp::And => self.op_and(addr_m),
-            InsGrp::Bit => self.op_bit(addr_m),
-            InsGrp::Brk => self.op_brk(),
-            InsGrp::CJump => self.op_jump(*flag_m, *arg),
-            InsGrp::Cmp => self.op_cmp(addr_m, reg_m),
-            InsGrp::Dec => self.op_dec(addr_m, reg_m),
-            InsGrp::Flag =>self.op_setfl(*flag_m, *arg),
-            InsGrp::Inc => self.op_inc(addr_m, reg_m),
-            InsGrp::Jsr => self.op_jsr(),
-            InsGrp::Jump =>self.op_jmp(addr_m ),
-            InsGrp::Load =>self.op_load(addr_m, reg_m),
-            InsGrp::Nop => self.op_nop(),
-            InsGrp::Or =>  self.op_or(addr_m),
-            InsGrp::Pull =>self.op_pull(reg_m),
-            InsGrp::Push =>self.op_push(reg_m),
-            InsGrp::Rot => self.op_rot(addr_m, *arg),
-            InsGrp::Rti => self.op_rti(),
-            InsGrp::Rts => self.op_rts(),
-            InsGrp::Sbc => self.op_sbc(addr_m),
-            InsGrp::Shift => self.op_shift(addr_m, *arg),
-            InsGrp::Store => self.op_store(addr_m, reg_m),
-            InsGrp::Tx_from_A => self.op_tx(&RegMod::A, reg_m),
-            InsGrp::Tx_from_SP =>self.op_tx(&RegMod::SP, reg_m),
-            InsGrp::Tx_from_X => self.op_tx(&RegMod::X, reg_m),
-            InsGrp::Tx_from_Y => self.op_tx(&RegMod::Y, reg_m),
-            InsGrp::Xor => self.op_xor(addr_m),
-            InsGrp::Invalid => self.op_hlt(),
-        };        
-
-        self.clk_count += clk;
-        clk
-        */        
     }
 
     // Gets instruction length by addressing mode
     //
     pub fn get_instr_length(&self, addrm: &AddrMode) -> u32 {
         match *addrm {
-            AddrMode::Imm |
-            AddrMode::ZP |
-            AddrMode::ZPX |
-            AddrMode::ZPY |
-            AddrMode::ZPIndX |
-            AddrMode::ZPIndY |
-            AddrMode::Rel => 2,
+            AddrMode::Imm | AddrMode::ZP | AddrMode::ZPX | AddrMode::ZPY | AddrMode::ZPIndX |
+            AddrMode::ZPIndY | AddrMode::Rel => 2,
             AddrMode::Abs | AddrMode::AbsX | AddrMode::AbsY | AddrMode::Ind => 3,
-            _ => 1
+            _ => 1,
         }
     }
 
     //
     // Fetch instruction, and updates program counter
     //
-    fn fetch_instr(&mut self)  {
+    fn fetch_instr(&mut self) {
         self.icd.opcode = self.mem.read_byte(self.regs.PC);
         self.regs.PC = self.regs.PC + 1;
     }
@@ -304,7 +272,7 @@ impl<'a> Cpu<'a> {
             RegMod::Y => self.regs.Y = v,
             RegMod::SR => self.regs.SR = v,
             RegMod::SP => self.regs.SP = v,
-            _ => panic!("invalid Reg modifier")
+            _ => panic!("invalid Reg modifier"),
         };
     }
 
@@ -315,12 +283,12 @@ impl<'a> Cpu<'a> {
             RegMod::Y => self.regs.Y,
             RegMod::SR => self.regs.SR,
             RegMod::SP => self.regs.SP,
-            _ => panic!("invalid Reg modifier")
+            _ => panic!("invalid Reg modifier"),
         }
     }
 
     pub fn is_flag_on(&self, flag: u8) -> bool {
-        self.regs.SR & flag == flag 
+        self.regs.SR & flag == flag
     }
 
 
@@ -364,13 +332,9 @@ impl<'a> Cpu<'a> {
         }
     }
 
-    fn set_nz_flags(&mut self, flags: u8, v: u8) {
-        if FLAG_ZERO == (flags & FLAG_ZERO) {
-            self.set_z_flag(v);
-        }
-        if FLAG_SIGN == (flags & FLAG_SIGN) {
-            self.set_s_flag(v);
-        }
+    fn set_nz_flags(&mut self, v: u8) {
+        self.set_z_flag(v);
+        self.set_s_flag(v);
     }
 
     fn addr_from_2b(&self, b0: u8, b1: u8) -> u16 {
@@ -379,37 +343,50 @@ impl<'a> Cpu<'a> {
 
     fn calc_eff_addr(&mut self, addr_mode: &AddrMode) {
         match *addr_mode {
-            AddrMode::Acc | AddrMode::Impl => {},
-            AddrMode::Imm =>  self.icd.ea = self.regs.PC + 1,
-            AddrMode::ZP =>   self.icd.ea = self.icd.op0 as u16,
-            AddrMode::ZPX =>  self.icd.ea = self.icd.op0.wrapping_add(self.regs.X) as u16,
-            AddrMode::ZPY =>  self.icd.ea = self.icd.op0.wrapping_add(self.regs.Y) as u16,
-            AddrMode::Abs =>  self.icd.ea = self.addr_from_2b(self.icd.op0, self.icd.op1),
+            AddrMode::Acc | AddrMode::Impl => {}
+            AddrMode::Imm => self.icd.ea = self.regs.PC + 1,
+            AddrMode::ZP => self.icd.ea = self.icd.op0 as u16,
+            AddrMode::ZPX => self.icd.ea = self.icd.op0.wrapping_add(self.regs.X) as u16,
+            AddrMode::ZPY => self.icd.ea = self.icd.op0.wrapping_add(self.regs.Y) as u16,
+            AddrMode::Abs => self.icd.ea = self.addr_from_2b(self.icd.op0, self.icd.op1),
             AddrMode::AbsX => {
                 if (self.regs.X).overflowing_add(self.icd.op0).1 {
-                self.clk_count += 1;
-            };
-            
-                self.icd.ea = (self.regs.X as u16).wrapping_add(self.addr_from_2b(self.icd.op0, self.icd.op1));           
-            },
+                    self.clk_count += 1;
+                };
+
+                self.icd.ea = (self.regs.X as u16).wrapping_add(self.addr_from_2b(
+                    self.icd.op0,
+                    self.icd.op1,
+                ));
+            }
 
             AddrMode::AbsY => {
                 if (self.regs.Y).overflowing_add(self.icd.op0).1 {
                     self.clk_count += 1;
                 };
-                self.icd.ea = (self.regs.Y as u16).wrapping_add(self.addr_from_2b(self.icd.op0, self.icd.op1));                    
-                },
-            
-            AddrMode::ZPIndX => self.icd.ea = self.addr_from_2b(
-                    self.mem
-                        .read_byte(self.regs.X.wrapping_add(self.icd.op0) as u16),
-                    self.mem
-                        .read_byte(self.regs.X.wrapping_add(1).wrapping_add(self.icd.op0) as u16)),
+                self.icd.ea = (self.regs.Y as u16).wrapping_add(self.addr_from_2b(
+                    self.icd.op0,
+                    self.icd.op1,
+                ));
+            }
+
+            AddrMode::ZPIndX => {
+                self.icd.ea = self.addr_from_2b(
+                    self.mem.read_byte(
+                        self.regs.X.wrapping_add(self.icd.op0) as u16,
+                    ),
+                    self.mem.read_byte(
+                        self.regs.X.wrapping_add(1).wrapping_add(
+                            self.icd.op0,
+                        ) as u16,
+                    ),
+                )
+            }
 
             AddrMode::ZPIndY => {
                 let indirect_addr = self.addr_from_2b(
                     self.mem.read_byte(self.icd.op0 as u16),
-                    self.mem.read_byte(self.icd.op0.wrapping_add(1) as u16)
+                    self.mem.read_byte(self.icd.op0.wrapping_add(1) as u16),
                 );
 
                 if ((indirect_addr & 0xFF) as u8)
@@ -418,16 +395,17 @@ impl<'a> Cpu<'a> {
                 {
                     self.clk_count += 1;
                 }
-                
-                self.icd.ea = indirect_addr.wrapping_add(self.regs.Y as u16);                    
-            },
+
+                self.icd.ea = indirect_addr.wrapping_add(self.regs.Y as u16);
+            }
             AddrMode::Ind => {
                 let indirect_addr = self.addr_from_2b(self.icd.op0, self.icd.op1);
                 self.icd.ea = self.addr_from_2b(
-                        self.mem.read_byte(indirect_addr),
-                        self.mem.read_byte(indirect_addr.wrapping_add(1)));
-                },
-            AddrMode::Rel => self.icd.ea = self.regs.PC + self.icd.op0 as u16
+                    self.mem.read_byte(indirect_addr),
+                    self.mem.read_byte(indirect_addr.wrapping_add(1)),
+                );
+            }
+            AddrMode::Rel => self.icd.ea = self.regs.PC + self.icd.op0 as u16,
         }
     }
 
@@ -447,8 +425,9 @@ impl<'a> Cpu<'a> {
             _ => self.mem.read_byte(self.icd.ea),
         }
     }
+}
 
-    // -----------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------
     //
     // Opcode implementations
     //
@@ -702,4 +681,3 @@ impl<'a> Cpu<'a> {
         0
     }
     */
-}
