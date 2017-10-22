c8f527200da4f59001410460c8724f966073c76c
Hernán Di Pietro <hernan.di.pietro@gmail.com>
2017-10-22 03:13:34 -0300

    Enhanced command handling

--- src/cmdint.rs
+++ src/cmdint.rs
@@ -1,13 +1,12 @@
 use std::io::*;
 use std::fs::*;
-use std::ascii::AsciiExt;
 use cpu;
 use cpu::opc6502::OPCODE_TABLE;
 
 #[derive(PartialEq)]
 pub enum Command {
     Asm,
-    Disasm { start_addr: Option<u16>, length: Option<u16> },
+    Disasm { start_addr: u16, length: u16 },
     Enter,
     Mem,
     Go,
@@ -15,7 +14,8 @@ pub enum Command {
     Null,
     Quit,
     ResetCPU,
-    Reg,    
+    Reg { reg: Option<String>, val: Option<u16> },    
+    Step
 }
 
 
@@ -33,6 +33,11 @@ pub fn do_prompt() -> Command {
         let cmdch = cmdentry.unwrap().to_uppercase().next(); 
         let args =  line.split_off(1);
         let argvec: Vec<&str> = if args.len() == 0 { Vec::new() } else { args.trim().split(",").collect() };
+
+        if argvec.len() > 0 && argvec[0] == "?" {
+            display_cmd_help(cmdch.unwrap());
+            return Command::Null;
+        }
                 
         match cmdch {
             Some('!') => {
@@ -60,19 +65,28 @@ pub fn do_prompt() -> Command {
             }
             Some('A') => return Command::Asm,
             Some('D') => {
-                if argvec.len() > 0 {
-                    if argvec[0] == "?" {
-                        println!("D[start_address][,len]\tDisassemble\n");
-                        println!("start_address\t\tThe memory address where disassembly starts. ");
-                        println!("             \t\tIf unspecified, current program counter is assumed.");
-                        println!("len          \t\tLength in bytes to dissasemble. Default is 32.");
-                        return Command::Null
-                    }
+
+                let mut startaddr:u16 = 0;
+                let mut len:u16 = 32;
+                let v = u16::from_str_radix(argvec[0], 16);
+                if v.is_err() {
+                    println!("?Invalid address: {}", argvec[0]);
+                    return Command::Null;
                 }
-                let start: Option<u16> = if argvec.len() > 0 { Some(argvec[0].parse::<u16>().unwrap()) } else { None };
-                let len: Option<u16> = if argvec.len() == 2 { Some(argvec[1].parse::<u16>().unwrap()) } else { None};
+                else {
+                    startaddr = v.unwrap();
+                }
+
+                let v = u16::from_str_radix(argvec[1], 16);
+                if v.is_err() {
+                    println!("?Invalid length: {}", argvec[1]);
+                    return Command::Null;
+                }
+                else {
+                    len = v.unwrap();
+                }   
            
-                return Command::Disasm { start_addr: start, length: len };
+                return Command::Disasm { start_addr: startaddr, length: len };
             },
             Some('E') => return Command::Enter,
             Some('G') => return Command::Go,
@@ -81,13 +95,6 @@ pub fn do_prompt() -> Command {
                     println!("?Filename not specified  error");
                     return Command::Null;
                 }     
-                
-                if argvec[0] == "?" {
-                    println!("L[filename][,addr]\tLoad program in memory\n");
-                    println!("filename\t\tBinary image with code to load. Should not exceed 64K.");
-                    println!("addr    \t\tAddress where the code is going to be located.");
-                    return Command::Null
-                } 
 
                 let mut startaddr:u16 = 0;
                 if argvec.len() > 1 {
@@ -103,10 +110,39 @@ pub fn do_prompt() -> Command {
                 return Command::Load{ filename: String::from(argvec[0]), start_addr: startaddr };
             },
             Some('M') => return Command::Mem,
-            Some('R') => return Command::Reg,
-            
-            Some('?') => println!("Help"),
-            _ => println!("?"),
+            Some('R') => {
+
+                if argvec.len() == 1 {
+                    println!("?Missing register value  error");
+                    return Command::Null;
+                }
+
+                let mut regname: Option<String> = None;
+                let mut regval:  Option<u16> = None;
+
+                if argvec.len() > 1 {
+                    let r = argvec[0].to_uppercase();
+                    if r != "X" && r != "SR" && r != "Y" && r != "A" && r != "PC" && r != "SP" {
+                        println!("?Invalid register. Use A,X,Y,PC,SP,SR");
+                        return Command::Null;
+                    }
+                    regname = Some(r);
+                    
+                    let ad = u16::from_str_radix(argvec[1], 16);
+                    if ad.is_err() {
+                        println!("?Invalid value: {}", argvec[1]);
+                        return Command::Null;
+                    }
+                    else {
+                        regval = Some(ad.unwrap());
+                    }
+                }    
+
+                return Command::Reg { reg: regname, val: regval };
+            }
+            Some ('S') => return Command::Step,            
+            Some ('?') => display_help(),
+            _ => println!("?Unknown command {}", cmdch.unwrap()),
         }
     }
 
@@ -120,23 +156,32 @@ pub fn exec(cmd: &Command, cpu: &mut cpu::Cpu) -> bool {
         },
         Command::ResetCPU => {
             cpu.reset();
-            exec(&Command::Reg, cpu);
+            exec(&Command::Reg { reg:None, val:None }, cpu);
         }
-        Command::Reg => {
-            println!("PC      N V - B D I Z C    AC  XR  YR  SP");
-            println!("{:04X}    {rN} {rV}   {rB} {rD} {rI} {rZ} {rC}    {ac:02X}  {xr:02X}  {yr:02X}  {sp:02X}", regs = cpu.regs.PC, 
-            ac = cpu.regs.A, xr = cpu.regs.X, yr = cpu.regs.Y, sp = cpu.regs.SP,
-            rB = if cpu.is_flag_on(cpu::FLAG_BRK) { '1' } else { '.' },
-            rN = if cpu.is_flag_on(cpu::FLAG_SIGN) { '1' } else { '.' },
-            rV = if cpu.is_flag_on(cpu::FLAG_OF) { '1' } else { '.' },
-            rD = if cpu.is_flag_on(cpu::FLAG_DEC) { '1' } else { '.' },
-            rI = if cpu.is_flag_on(cpu::FLAG_INTR) { '1' } else { '.' },
-            rZ = if cpu.is_flag_on(cpu::FLAG_ZERO) { '1' } else { '.' },
-            rC = if cpu.is_flag_on(cpu::FLAG_CARRY) { '1' } else { '.' });
+        Command::Reg { ref reg, ref val }=> {
+            if reg.is_none() {
+                println!("PC      N V - B D I Z C    AC  XR  YR  SP");
+                println!("{:04X}    {rN} {rV}   {rB} {rD} {rI} {rZ} {rC}    {ac:02X}  {xr:02X}  {yr:02X}  {sp:02X}", regs = cpu.regs.PC, 
+                ac = cpu.regs.A, xr = cpu.regs.X, yr = cpu.regs.Y, sp = cpu.regs.SP,
+                rB = if cpu.is_flag_on(cpu::FLAG_BRK) { '1' } else { '.' },
+                rN = if cpu.is_flag_on(cpu::FLAG_SIGN) { '1' } else { '.' },
+                rV = if cpu.is_flag_on(cpu::FLAG_OF) { '1' } else { '.' },
+                rD = if cpu.is_flag_on(cpu::FLAG_DEC) { '1' } else { '.' },
+                rI = if cpu.is_flag_on(cpu::FLAG_INTR) { '1' } else { '.' },
+                rZ = if cpu.is_flag_on(cpu::FLAG_ZERO) { '1' } else { '.' },
+                rC = if cpu.is_flag_on(cpu::FLAG_CARRY) { '1' } else { '.' });
+            } 
+            else {
+                match reg.as_ref() {
+                    Some("X") => {}
+                    _ => panic!("R command with unexpected regname")
+                }                                
+            }
         },
         Command::Disasm { start_addr, length } => {
-            let mut current_addr = if start_addr.is_some() { start_addr.unwrap() } else { 0 };
-            for _ in 0..10 {  
+            let mut current_addr = start_addr;
+            let mut byte_count: u32 = 0;
+            loop {  
                 let opcode_byte = cpu.mem.read_byte(current_addr);
                 let opcode_data = &OPCODE_TABLE[opcode_byte as usize];
                 let instr_len = cpu.get_instr_length(&opcode_data.addr_m);
@@ -171,6 +216,11 @@ pub fn exec(cmd: &Command, cpu: &mut cpu::Cpu) -> bool {
                     cpu::AddrMode::Impl   => format!("")                    
                 });
                 current_addr += instr_len as u16;
+                byte_count += instr_len;
+
+                if byte_count >= length as u32 {
+                    break;
+                }
             }
 
         },
@@ -273,4 +323,41 @@ fn ask_choice(text: &str, options: &Vec<char>) -> char {
             }
         }
     }
+}
+
+fn display_cmd_help(c: char) {
+    match c {
+        'D' => { println!("D[start_address][,len]\tDisassemble\n");
+                 println!("start_address\t\tThe memory address where disassembly starts. ");
+                 println!("             \t\tIf unspecified, current program counter is assumed.");
+                 println!("len          \t\tLength in bytes to dissasemble. Default is 32.");
+        }
+        'L' => { println!("L[filename][,addr]\tLoad program in memory\n");
+                 println!("filename\t\tBinary image with code to load. Should not exceed 64K.");
+                 println!("addr    \t\tAddress where the code is going to be located.");
+        }
+        'R' => { println!("R[reg,value]\tDisplay or modify registers\n");
+                 println!("reg,value\t\tRegistry with value to modify. If unspecified, displays");
+                 println!("         \t\tcurrent processor registers."); 
+        }
+        _ => { println!("No help for this command."); }
+        
+    }
+}
+
+fn display_help() {
+    println!("Commands: ");
+    println!("A         Assemble");
+    println!("D         Dissassemble");
+    println!("E         Enter values in memory");
+    println!("G         Go (execute CPU)");
+    println!("L         Load file");
+    println!("R         Display or modify registers");
+    println!("S         Step");
+    println!("----------------------------------------------------------------");
+    println!("Extended commands:");
+    println!("!LICENSE  Display program license text");
+    println!("!RESET    Reset CPU");
+    println!("!MODEL    Set CPU model (6502, 65C02 or 65CE02)");
+    println!("!CLOCK    Set CPU clock in MHz");
 }
